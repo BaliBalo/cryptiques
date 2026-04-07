e19f5930fbfabc9e0f5b45a26c009d5e5e796190
BaliBalo <bali@balo.cool>
2026-04-07 10:55:42 +0100

    Some SEO stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
+	site: {
+		url: 'cryptiques.balibalo.xyz',
+		name: 'Cryptiques',
+		indexable: true,
+	},
 	// spaLoadingTemplate: false,
 	devtools: { enabled: false },
 	app: {
 		head: {
+			title: 'Cryptiques',
+			htmlAttrs: { lang: 'fr' },
 			link: [
 				{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 				{ rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
