a508d629a71bcb22fc5bd457b5397294450979e5
BaliBalo <bali@balo.cool>
2026-05-16 06:44:45 +0100

    Update privacy policy

--- app/pages/politique-de-confidentialite.vue
+++ app/pages/politique-de-confidentialite.vue
@@ -7,11 +7,12 @@
 
 <template>
 	<main>
+		<Back />
 		<h1>Politique de confidentialité</h1>
-		<small>Dernière mise à jour : <time datetime="2026-05-05">5 mai 2026</time></small>
+		<small>Dernière mise à jour : <time datetime="2026-05-16">16 mai 2026</time></small>
 		<hr>
 		<h2>Données personelles collectées</h2>
-		<p>Lorsque vous vous connectez à un compte via un service tiers, nous enregistrons votre adresse e-mail comme identifiant pour votre compte.</p>
+		<p>Lorsque vous vous connectez à un compte, nous enregistrons votre adresse e-mail comme identifiant pour votre compte.</p>
 		<p>Sur votre navigateur, des cookies sont utilisés pour retenir vos préférences ainsi que le compte auquel vous êtes connecté.</p>
 		<hr>
 		<h2>Utilisation des données</h2>
@@ -21,7 +22,7 @@
 		<p>Les données personnelles sont conservées aussi longtemps que nécessaire pour le bon fonctionnement du site.</p>
 		<hr>
 		<h2>Suppression des données</h2>
-		<p>Vous pouvez demander la suppression de vos données personnelles par email via le <NuxtLink to="/contact">formulaire de contact</NuxtLink>.</p>
+		<p>Vous pouvez demander la suppression de votre compte et des données personnelles associées via le <NuxtLink to="/contact">formulaire de contact</NuxtLink>.</p>
 	</main>
 </template>
 
