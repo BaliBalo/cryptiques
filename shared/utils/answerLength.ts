590fc48ab2af4b316fad72bc6ee8dd49496a6ef6
BaliBalo <bali@balo.cool>
2026-06-20 21:36:56 +0100

    Ignore multiple separators in a row

--- shared/utils/answerLength.ts
+++ shared/utils/answerLength.ts
@@ -3,12 +3,12 @@ const separators = {
 	'-': '-',
 	"'": "'",
 };
-export const isSeparator = (str: string): str is keyof typeof separators => str in separators;
+export const isSeparator = (str: string | undefined): str is keyof typeof separators => !!str && str in separators;
 
 const escapeInCharacterClass = ['^', '-', ']', '\\'];
-export const separatorsRegex = new RegExp(`([${Object.keys(separators).map(s => escapeInCharacterClass.includes(s) ? `\\${s}` : s).join('')}])`, 'g');
+export const separatorsRegex = new RegExp(`([${Object.keys(separators).map(s => escapeInCharacterClass.includes(s) ? `\\${s}` : s).join('')}]+)`, 'g');
 
 export function getAnswerLength(answer: string) {
-	const parts = answer.split(separatorsRegex).map(part => isSeparator(part) ? separators[part] : part.length);
+	const parts = answer.split(separatorsRegex).map(part => isSeparator(part[0]) ? separators[part[0]] : part.length);
 	return `(${parts.join('')})`;
 }
